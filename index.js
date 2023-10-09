ba0fc7b8c358a1ee4cc93d4ac29e1faed13eaade
Droxus <alexredov@gmail.com>
2023-10-09 19:42:39 +0300

    +fetch handler

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 require("dotenv").config();
 
 console.log(process.env.GREETING);
+
+async function scrapePage() {
+    const response = await fetch('https://github.com/trending');
+    const html = await response.text();
+    console.log(html);
+    if (!response.ok) return console.error('Failed to delete');
+}
+
+scrapePage();
